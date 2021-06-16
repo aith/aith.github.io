ee80b1d1832769866a07a378580766ae3ada2c6c
aith <ariiramanesh@gmail.com>
2021-06-15 20:27:21 -0700

    revert

--- assets/js/mansory.js
+++ assets/js/mansory.js
@@ -1,28 +1,15 @@
 // For the projects
-// Init Masonry
-/* https://stackoverflow.com/questions/30888852/masonry-imagesloaded-html5-video-poster */
-var posterCount = $('video').length;
-var postersLoaded = 0;
-
-
-var $grid = $('.grid').masonry({
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  columnWidth: '.grid-sizer',  // Make sure there's a div named grid-sizer placed in the html
-});
-
-function ready_up() {
-    postersLoaded++;
-    if (postersLoaded >= posterCount) {
-        /* Wait for images to load */
-        $grid.imagesLoaded().progress( function() {
-          $('grid').masonry('reloadItems');
-          $('grid').masonry('layout');
-        });
-    }
-}
-
-var videoCollection = document.getElementsByTagName("video");
-Array.from(videoCollection).forEach(function (element) {
-  ready_up();
+$(document).ready(function() {
+  // Init Masonry
+  var $grid = $('.grid').masonry({
+    // gutter: '.gutter-sizer',
+    // horizontalOrder: true,
+    itemSelector: '.grid-item',
+    percentPosition:true,
+    columnWidth: '.grid-sizer',  // Make sure there's a div named grid-sizer placed in the html
+  });
+  // Layout Masonry after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry('layout');
+  });
 });
